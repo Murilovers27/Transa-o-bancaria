490c9bd1c1db0c52a552f8a0ba20370c3459f5be
Murilo Pires Andrade Cruz <murilopiresandradecruz27@gmail.com>
2025-12-06 00:38:04 +0000

    e la vamos nós

---
+++ front-end/src/app/features/cadastro-movimentacao/cadastro-movimentacao.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-movimentacao',
+  imports: [],
+  templateUrl: './cadastro-movimentacao.html',
+  styleUrl: './cadastro-movimentacao.css',
+})
+export class CadastroMovimentacao {
+
+}
---
+++ front-end/src/app/pages/lista-movimentacoes/lista-movimentacoes.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-movimentacoes',
+  imports: [],
+  templateUrl: './lista-movimentacoes.html',
+  styleUrl: './lista-movimentacoes.css',
+})
+export class ListaMovimentacoes {
+
+}
